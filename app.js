560eb49ef1ea1b77aa5a44d33ef2792e6af656ec
Cassiano Kuplich <crkuplich@openmailbox.org>
2017-12-15 20:46:39 -0200

    Add Apache License

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2017 Cassiano Kuplich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 require('dotenv').config()
 
 const path = require('path')
@@ -17,6 +33,7 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
+// GET /[latitude],[longitude]
 app.get(/^\/(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?)$/, function (req, res, next) {
   var latitude = req.params[0]
   var longitude = req.params[1]
@@ -41,6 +58,7 @@ app.get(/^\/(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?)$/, function (req, res, next) {
     })
 })
 
+// 404 error
 app.use(function (req, res) {
   res.status(404).render('404')
 })
